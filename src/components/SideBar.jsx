c013df422b7d3ea65e711d20ffb48e8f9b9e0de2
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-05-01 10:25:01 +0530

    Added SideBar component

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,21 @@
+const SideBar = ()=>{
+    return(
+        <aside>
+            <ul>
+                <a href="">
+                    <li>Collections</li>
+                </a><a href="">
+                    <li>Men</li>
+                </a><a href="">
+                    <li>Women</li>
+                </a><a href="">
+                    <li>About</li>
+                </a><a href="">
+                    <li>Contact</li>
+                </a>
+            </ul>
+        </aside>
+    )
+}
+
+export default SideBar
