d1e0892201a61b30ebda6a464d824e91b467ff5a
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-05-01 15:04:13 +0530

    Added the tranlucent background to the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,25 @@
 import close from "../assets/icon-close.svg"
 const Sidebar = ({CloseSidebar})=>{
     return(
-        <aside className="fixed top-0 w-[65%] h-full p-4 bg-red-900">
-            <button onClick={CloseSidebar}>
-                <img src={close} alt="Close Sidebar" />
-            </button>
-            <ul className="mt-8 font-semibold">
-                <a href="">
-                    <li>Collections</li>
-                </a><a href="">
-                    <li className="mt-2">Men</li>
-                </a><a href="">
-                    <li className="mt-2">Women</li>
-                </a><a href="">
-                    <li className="mt-2">About</li>
-                </a><a href="">
-                    <li className="mt-2">Contact</li>
-                </a>
-            </ul>
+        <aside className="fixed top-0 w-full h-full bg-black/75">
+            <div className="w-[65%] h-full p-4 bg-white">
+                <button onClick={CloseSidebar}>
+                    <img src={close} alt="Close Sidebar" />
+                </button>
+                <ul className="mt-8 font-semibold">
+                    <a href="">
+                        <li>Collections</li>
+                    </a><a href="">
+                        <li className="mt-2">Men</li>
+                    </a><a href="">
+                        <li className="mt-2">Women</li>
+                    </a><a href="">
+                        <li className="mt-2">About</li>
+                    </a><a href="">
+                        <li className="mt-2">Contact</li>
+                    </a>
+                </ul>
+            </div>
         </aside>
     )
 }
