d81a47640be49f3bf947a3b8305f7ca714b56769
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-05-02 15:45:26 +0530

    Made Navbar to stick to the top of the viewport

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,7 +11,7 @@ const Nav = () => {
     const toggleSidebar = ()=>setIsOpenSidebar(oldIsOpenSidebar => !oldIsOpenSidebar)
     return (
         <>
-            <nav className="flex items-center justify-between px-4 py-3">
+            <nav className="sticky top-0 z-20 flex items-center justify-between px-4 py-3 bg-white">
                 <div className="flex items-center">
                     <button onClick={toggleSidebar}>
                         <img src={menu} className="pr-3" alt="Menu" />
