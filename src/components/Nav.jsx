d027c232372c03f5093646dcfb5e3e970f9a1927
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-05-05 12:13:26 +0530

    Removed console log code

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,6 @@ import { useState } from "react"
 
 const Nav = () => {
     const [isOpenSidebar, setIsOpenSidebar] = useState(false)
-    console.log(isOpenSidebar)
     const toggleSidebar = () => setIsOpenSidebar(oldIsOpenSidebar => !oldIsOpenSidebar)
     return (
         <>
