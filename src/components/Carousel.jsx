57161519f0509eaf4382b1aa031dd03711b3e64d
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-05-05 12:08:41 +0530

    Added Carousel effect for the mobile product images

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,14 +1,31 @@
 import product1 from "../assets/image-product-1.jpg"
+import product2 from "../assets/image-product-2.jpg"
+import product3 from "../assets/image-product-3.jpg"
+import product4 from "../assets/image-product-4.jpg"
 import numbnail1 from "../assets/image-product-1-thumbnail.jpg"
 import numbnail2 from "../assets/image-product-2-thumbnail.jpg"
 import numbnail3 from "../assets/image-product-3-thumbnail.jpg"
 import numbnail4 from "../assets/image-product-4-thumbnail.jpg"
 import next from "../assets/icon-next.svg"
 import previous from "../assets/icon-previous.svg"
+import { useState } from "react"
+
 const Carousel = () => {
 
-    const numbnails = [numbnail1, numbnail2, numbnail3, numbnail4]
-    const renderedThumbnails = numbnails.map(pic => {
+    const [currentImage, setCurrentImage] = useState(0)
+
+    const productImages = [product1,product2,product3,product4]
+
+    const nextImage = ()=>setCurrentImage(oldCurrentImage=>{
+        return oldCurrentImage === 3 ? 0 : oldCurrentImage + 1
+    })
+
+    const prevImage = ()=>setCurrentImage(oldCurrentImage=>{
+        return oldCurrentImage === 0 ? 3 : oldCurrentImage - 1
+    })
+
+    const thumbnails = [numbnail1, numbnail2, numbnail3, numbnail4]
+    const renderedThumbnails = thumbnails.map(pic => {
         return (
             <img key={pic} src={pic} className="rounded-md scale-90" alt="Product Preview" />
         )
@@ -16,14 +33,14 @@ const Carousel = () => {
 
     return (
         <div className="max-w-[25rem] mx-auto max-[650px]:relative">
-            <button className="absolute top-[50%] left-3 rounded-full py-2 px-[0.65rem] bg-white min-[650px]:hidden">
+            <button onClick={prevImage} className="absolute top-[50%] left-3 rounded-full py-2 px-[0.65rem] bg-white min-[650px]:hidden">
                 <img src={previous} alt="Previous Image" />
             </button>
 
-            <img className="max-h-[25rem] mx-auto min-[650px]:rounded-lg" src={product1} />
+            <img className="max-h-[25rem] mx-auto min-[650px]:rounded-lg" src={productImages[currentImage]} />
             <div className="hidden grid-cols-4 gap-4 mt-4 min-[650px]:grid">{renderedThumbnails}</div>
 
-            <button className="absolute top-[50%] right-3 rounded-full py-2 px-[0.65rem] bg-white min-[650px]:hidden">
+            <button onClick={nextImage} className="absolute top-[50%] right-3 rounded-full py-2 px-[0.65rem] bg-white min-[650px]:hidden">
                 <img src={next} alt="Next Image" />
             </button>
         </div>
