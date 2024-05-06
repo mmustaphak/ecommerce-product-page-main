d833cbc3d2c3a99db4a660f3d8f719879aad91ea
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-05-06 15:43:27 +0530

    Synced the thumbnail images with the displayed product image

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -14,34 +14,42 @@ const Carousel = () => {
 
     const [currentImage, setCurrentImage] = useState(0)
 
-    const productImages = [product1,product2,product3,product4]
+    const productImages = [product1, product2, product3, product4]
 
-    function moveImage(isNext){
-        if(isNext){
+    function moveImage(isNext) {
+        if (isNext) {
             setCurrentImage(oldCurrentImage => oldCurrentImage === 3 ? 0 : oldCurrentImage + 1)
-        }else{
+        } else {
             setCurrentImage(oldCurrentImage => oldCurrentImage === 0 ? 3 : oldCurrentImage - 1)
         }
     }
 
-    const renderedThumbnails = [numbnail1, numbnail2, numbnail3, numbnail4].map(pic => {
-        return (
-            <img key={pic} src={pic} className="rounded-md scale-90" alt="Product Preview" />
-        )
+    const renderedThumbnails = [numbnail1, numbnail2, numbnail3, numbnail4].map((pic, index) => {
+        if (currentImage == index) {
+            return (
+                <div key={pic} className="border-2 border-orange rounded-lg scale-90">
+                    <img src={pic} className="rounded-md opacity-30" alt="Product Preview" />
+                </div>
+            )
+        } else {
+            return(
+                <img key={pic} src={pic} className="rounded-md scale-90" alt="Product Preview" />
+            )
+        }
     })
 
 
 
     return (
         <div className="max-w-[25rem] mx-auto max-[650px]:relative">
-            <button onClick={()=>moveImage(false)} className="absolute top-[50%] left-3 rounded-full py-2 px-[0.65rem] bg-white min-[650px]:hidden">
+            <button onClick={() => moveImage(false)} className="absolute top-[50%] left-3 rounded-full py-2 px-[0.65rem] bg-white min-[650px]:hidden">
                 <img src={previous} alt="Previous Image" />
             </button>
 
             <img className="max-h-[25rem] mx-auto min-[650px]:rounded-lg" src={productImages[currentImage]} />
             <div className="hidden grid-cols-4 gap-4 mt-4 min-[650px]:grid">{renderedThumbnails}</div>
 
-            <button onClick={()=>moveImage(true)} className="absolute top-[50%] right-3 rounded-full py-2 px-[0.65rem] bg-white min-[650px]:hidden">
+            <button onClick={() => moveImage(true)} className="absolute top-[50%] right-3 rounded-full py-2 px-[0.65rem] bg-white min-[650px]:hidden">
                 <img src={next} alt="Next Image" />
             </button>
         </div>
