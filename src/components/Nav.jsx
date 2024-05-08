ed196cc8ab767e7cfa95ac9cf2d756283f684b31
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-05-08 08:41:45 +0530

    Added basic code for the cart

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -37,6 +37,13 @@ const Nav = () => {
                     <svg width="22" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fill="#69707D" fill-rule="nonzero" /></svg>
                     <img src={avatar} className="max-h-[1.5rem] pl-5 min-[650px]:max-h-[2.5rem]" alt="Image Avatar" />
                 </div>
+                <div className="absolute flex flex-col w-[92%] min-h-64 right2/4 -bottom-[475%] rounded-lg bg-white">
+                    <h2 className="px-4 py-4 font-semibold">Cart</h2>
+                    <hr />
+                    <div className="m-auto">
+                        <p className="font-semibold text-sm text-dark-grayish-blue">Your cart is empty.</p>
+                    </div>
+                </div>
             </nav>
             {isOpenSidebar && <Sidebar CloseSidebar={toggleSidebar} />}
         </>
