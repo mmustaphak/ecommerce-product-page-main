c4a137ff7b61698369da0c0867bde25e76e269d7
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-05-11 16:33:25 +0530

    Added extra space between the nav and main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import Carousel from "./Carousel"
 import ProductDetails from "./ProductDetails"
 const Main = ({SetCartQuantity}) => {
     return (
-        <main className="min-[650px]:grid grid-cols-2 items-center min-[650px]:pt-2 min-[650px]:px-4 min-[650px]:gap-x-4">
+        <main className="min-[650px]:grid grid-cols-2 items-center min-[650px]:mt-8 min-[650px]:px-4 min-[650px]:gap-x-4">
             <Carousel />
             <ProductDetails 
                 SetCartQuantity = {SetCartQuantity}
